f21e58bf035215ebddeb1cbe032236ae15ba3fdd
unknown <ekobudi029@gmail.com>
2023-07-26 21:21:03 +0700

    update how to upload multiple image

--- app/Http/Controllers/API/TeamController.php
+++ app/Http/Controllers/API/TeamController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\API;
 
+use Image;
 use Exception;
 use App\Models\Team;
+
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\CreateTeamRequest;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\UpdateTeamRequest;
 
 class TeamController extends Controller
@@ -48,27 +52,53 @@ class TeamController extends Controller
         );
     }
 
-    public function create(CreateTeamRequest $request)
+
+    public function create(Request $request)
     {
         try {
-            if ($request->hasFile('icon')) {
-                $path = $request->file('icon')->store('public/icons');
-            }
-
-            $team = Team::create([
-                'name' => $request->name,
-                'icon' =>  isset($path) ? $path : '',
-                'company_id' => $request->company_id
+            $validator = Validator::make($request->all(), [
+                'icon.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+                'name' => 'required|string|max:255',
             ]);
 
-            if (!$team) {
-                throw new Exception('team Nout Found');
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors(),
+                ], 422);
             }
 
+            $name = $request->input('name');
+            $teamIcons = [];
 
-            return ResponseFormatter::success($team, 'team Created');
+            if ($request->hasFile('icon')) {
+                foreach ($request->file('icon') as $image) {
+                    $imageName = time()  . rand(1, 100) . '.' . $image->getClientOriginalExtension();
+
+                    $resizedImage = Image::make($image)->resize(850, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                    });
+                    $resizedImage->save(public_path('files/' . $imageName), 90);
+
+                    $teamIcons[] = $imageName;
+                }
+            }
+
+            $iconString = implode(',', $teamIcons);
+
+            $team = Team::create([
+                'name' => $name,
+                'icon' => $iconString,
+                'company_id' => 15,
+            ]);
+
+            $response = [
+                'message' => 'Data berhasil ditambahkan.',
+                'data' => $team,
+            ];
+
+            return response()->json($response, 201);
         } catch (\Throwable $th) {
-            return ResponseFormatter::error($th->getMessage(), 500);
+            return response()->json(['error' => 'Data gagal ditambahkan.'], 500);
         }
     }
 
@@ -114,3 +144,39 @@ class TeamController extends Controller
         }
     }
 }
+
+
+
+//single image
+
+
+// $request->validate([
+//     'icon' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+//     'name' => 'required|string|max:255',
+// ]);
+
+// $name = $request->input('name');
+
+// // Simpan gambar menggunakan store()
+// $image = $request->file('icon');
+// $imageName = time()  . rand(1, 100) . '.' . $image->getClientOriginalExtension();
+
+// $resizedImage = Image::make($image)->resize(850, null, function ($constraint) {
+//     $constraint->aspectRatio();
+// });
+// $resizedImage->save(public_path('files/' . $imageName), 90);
+
+// $team = Team::create([
+//     'name' => $name,
+//     'icon' => $imageName,
+//     'company_id' => 15,
+// ]);
+
+// $team->save();
+
+// $response = [
+//     'message' => 'Data berhasil ditambahkan.',
+//     'data' => $team,
+// ];
+
+// return response()->json($response, 201);
